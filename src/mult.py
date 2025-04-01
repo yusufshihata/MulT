c09cb2d8598070cead0fe4b6bdb17bc54a67ff61
Yusuf Shihata <yusufshihata2006@gmail.com>
2025-04-01 12:25:35 +0200

    finishing the crossmodal transformer layer

--- src/mult.py
+++ src/mult.py
@@ -9,7 +9,7 @@ class TempConv(nn.Module):
         self.conv1d = nn.Conv1d(in_channels=channels, out_channels=channels, kernel_size=kernel_size, padding=kernel_size//2)
 
     def forward(self, x):
-        x = x.permute(1, 0, 2)
+        x = x.unsqueeze(0).permute(1, 0, 2)
         x = self.conv1d(x)
         return x.permute(1, 0, 2).squeeze(0)
 
@@ -31,23 +31,24 @@ class PositionalEncoding(nn.Module):
         self.register_buffer('pe', pe)
         
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        assert x.shape == torch.Size([self.seq_len, self.feature_dim]), "Vector Shape is incorrect"
+        assert x.shape == torch.Size([self.seq_len, self.feature_dim]), "Tensor Shape is incorrect"
         return x + self.pe
 
-class CrossModalTransformer(nn.Module):
-    def __init__(self, primary_in_shape: List[int], secondary_in_shape: List[int], primary_feature_dim: int, secondary_feature_dim: int, dims: int):
-        super(CrossModalTransformer, self).__init__()
+class CrossModalTransformerLayer(nn.Module):
+    def __init__(self, primary_in_shape: List[int], secondary_in_shape: List[int], dims: int):
+        super(CrossModalTransformerLayer, self).__init__()
         self.dims = dims
         self.primary_ln = nn.LayerNorm(primary_in_shape)
         self.secondary_ln = nn.LayerNorm(secondary_in_shape)
-        self.primary_proj = nn.Linear(in_features=primary_feature_dim, out_features=dims)
-        self.secondary_proj = nn.Linear(in_features=secondary_feature_dim, out_features=dims*2)
+        self.primary_proj = nn.Linear(in_features=primary_in_shape[1], out_features=dims)
+        self.secondary_proj = nn.Linear(in_features=primary_in_shape[1], out_features=dims*2)
         self.softmax = nn.Softmax()
 
     def attention(self, Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor) -> torch.Tensor:
         atten = (Q @ K.transpose(-1, -2)) / math.sqrt(self.dims)
         atten = self.softmax(atten)
-        return atten * V
+        print(atten.shape, V.shape)
+        return atten @ V
 
     def forward(self, primary: torch.Tensor, secondary: torch.Tensor) -> torch.Tensor:
         primary = self.primary_ln(primary)
@@ -57,8 +58,7 @@ class CrossModalTransformer(nn.Module):
         K, V = torch.split(KV, KV.size(1) // 2, dim=1)
 
         atten = self.attention(Q, K, V)
+
         return atten + primary
 
-if __name__ == "__main__":
-    pass
 
